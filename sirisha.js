a8856901ceb124e5507155248353014b769d3fee
sirisharanikalapala <165934666+sirisharanikalapala@users.noreply.github.com>
2024-04-26 14:00:05 +0530

    Update sirisha.js

--- sirisha.js
+++ sirisha.js
@@ -1,11 +1,11 @@
 alert("Start again?");
 var randomNumber= Math.floor(Math.random()*6)+1;
-var imageSource="images/dice"+randomNumber +".png";
+var imageSource="./dice"+randomNumber +".png";
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src",imageSource);
 
 var randomNumber2= Math.floor(Math.random()*7)+1;
-var imageSource2="images/dice"+randomNumber2 +".png";
+var imageSource2="./dice"+randomNumber2 +".png";
 var image2= document.querySelectorAll("img")[1];
 image2.setAttribute("src",imageSource2);
 if(randomNumber===randomNumber2){
